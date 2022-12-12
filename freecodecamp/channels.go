e3f87bc81f1373bbc52cfcb388ba4c8499da6c96
trinhnv <trinhnguyen959@gmail.com>
2022-12-12 21:24:29 +0700

    change folder structure

--- freecodecamp/channels.go
+++ freecodecamp/channels.go
@@ -10,16 +10,18 @@ var wgc = sync.WaitGroup{}
 func main() {
 	ch := make(chan int)
 	wgc.Add(2)
-	go func() {
-		i := <-ch
-		fmt.Println(i)
+	go func(ch <-chan int) {
+		for i := range ch {
+			fmt.Println(i)
+		}
 		wgc.Done()
-	}()
+	}(ch)
 
-	go func() {
+	go func(ch chan<- int) {
 		ch <- 42
-		fmt.Println(<-ch)
+		ch <- 27
+		close(ch)
 		wgc.Done()
-	}()
+	}(ch)
 	wgc.Wait()
 }
