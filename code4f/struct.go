d0a21e16a7f2407a1d81e50a0af8a54a000064b3
trinhnv <trinhnguyen959@gmail.com>
2022-12-18 11:07:53 +0700

    add struct

--- code4f/struct.go
+++ code4f/struct.go
@@ -33,9 +33,68 @@ func main() {
 	}{101112, "Cy2"}
 
 	fmt.Println(st4)
+
+	// anonymous struct
+	var anonymous = struct {
+		email string
+		age   int
+	}{
+		email: "trinhnv@gmail.com",
+		age:   31,
+	}
+	fmt.Println(anonymous)
+
+	// pointer tro toi struct
+	st5 := &Student{
+		789, "robinson",
+	}
+	fmt.Println()
+	fmt.Println(&st5) // print pointer
+	fmt.Println((*st5).id)
+	fmt.Println(st5.id)
+
+	// struct in struct, nested struct
+	student := Students{
+		id:   123,
+		name: "This",
+		info: StudentInfo{
+			class: "0101",
+			email: "this@gmail.com",
+			age:   27,
+		},
+	}
+	fmt.Println()
+	fmt.Println(student)
+	fmt.Println()
+
+	// compare 2 struct
+	s1 := struct1{1, "A"}
+	s2 := struct1{1, "A"}
+	fmt.Println(s1 == s2)
+
+	// zero value
+	var student2 Students
+	fmt.Println(student2)
+}
+
+type struct1 struct {
+	id   int
+	name string
 }
 
 type Student struct {
 	id   int
 	name string
+}
+
+type Students struct {
+	id   int
+	name string
+	info StudentInfo
+}
+
+type StudentInfo struct {
+	class string
+	email string
+	age   int
 }
